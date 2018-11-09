d3f3573c112c0301c0ff6fda20f95793a22aeb3c
tianyouyou <tianyouy@163.com>
2018-11-09 16:33:02 +0800

    猫眼

---
+++ Game/myapp/src/components/common/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <ul class="footer">
+            <li>
+            电影
+            </li>
+            <li>
+            音乐
+            </li>
+            <li>
+            书籍
+            </li>
+            <li>
+            图片
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.footer{height: 1rem;width: 100%;color:#fff;background: #2196f3;position: fixed;bottom: 0;line-height: 1rem;display: flex;justify-content:space-evenly;}
+/* .footer li{width: 25%;text-align: center;} */
+</style>
